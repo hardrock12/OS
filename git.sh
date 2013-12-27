01bc6a6ab79e93f8573b96fa978d9a4af89c50ec
Arjun <123arjunsuresh@gmail.com>
2013-12-27 13:31:24 +0530

    ([A[A[B)

---
+++ git.sh
@@ -0,0 +1,8 @@
+rm *.*~
+git status
+read commit
+git add .
+git commit -m "($commit)"
+git log
+
+git push gcode master
---
+++ readme.txt
@@ -0,0 +1,8 @@
+REQUIREMENT
+====================
+
+1. gcc
+2. linker
+3. VirtualBox
+4. grub-mkrescue
+5. nasm
